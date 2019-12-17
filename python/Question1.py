583fe7f149103249a8b3e765aaee87a559d6387c
isla rose perfito <islaperfito@gmail.com>
2019-12-17 13:08:36 -0500

    question 1

--- python/Question1.py
+++ python/Question1.py
@@ -1,25 +1,13 @@
 # modify this function, and create other functions below as you wish
 def question01(initialLevelOfDebt, interestPercentage, repaymentPercentage):
     monthlyRepayment = (initialLevelOfDebt * repaymentPercentage) / 100
-
     currentDebt = initialLevelOfDebt
-
     interestPercentage = 1 + interestPercentage / 100
-
     count = 0
 
     while currentDebt > monthlyRepayment:
-
         currentDebt = currentDebt * interestPercentage - monthlyRepayment;
         count+=1
-
-
-        # 50 = 1000 * 0.05
-
-        #print(currentDebt, interest, currentDebt+interest, currentDebt + interest - monthlyRepayment)
-
-
-
     return round(count * monthlyRepayment + currentDebt + 0.1 * initialLevelOfDebt)
 
-print(question01(1000, 0.05, 0.10))
+#print(question01(1000, 0.05, 0.10))
